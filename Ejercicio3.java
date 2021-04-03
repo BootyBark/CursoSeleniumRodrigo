e24749ff9858f71ed5909965987677917f5e6874
DESKTOP-C9VUMRH\Rodrigo <rodrigoituarte22@gmail.com>
2021-04-03 08:25:12 -0300

    este es el ejercicio 3 del practico 8

---
+++ Ejercicio3.java
@@ -0,0 +1,10 @@
+package practico3;
+
+public class Ejercicio3 {
+    public static void main (String args[]){
+        System.out.println(reciboNumeroYRetornoDoble(5.3));
+    }
+    public static double reciboNumeroYRetornoDoble (double numero){
+        return (numero *2);
+    }
+}
